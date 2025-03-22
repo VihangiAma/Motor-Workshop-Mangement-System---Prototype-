ae87a272927f63e7f212cdf7a84a415a37899c9e
K.V.A.T Nishshanka <129648585+kvat001@users.noreply.github.com>
2025-03-22 21:01:11 +0530

    Update home page

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
         document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
     });
 });
+let slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+            let slides = document.getElementsByClassName("slide");
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}
+            slides[slideIndex - 1].style.display = "block";
+            setTimeout(showSlides, 3000); // Change image every 3 seconds
+        }
+
+        function plusSlides(n) {
+            slideIndex += n - 1;
+            showSlides();
+        }
