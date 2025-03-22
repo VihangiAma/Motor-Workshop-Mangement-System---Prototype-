13aadf076b1d56307e43dee25cffb094c52efd9d
K.V.A.T Nishshanka <129648585+kvat001@users.noreply.github.com>
2025-03-22 12:55:48 +0530

    content of css styles

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const sectionId = this.getAttribute('href').substring(1);
+        document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
